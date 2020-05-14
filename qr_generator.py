35f581d1a0bf1b2ef7662360fe6ce970baa9f3e5
Francesca Vianello <francesca.vianello11@imperial.ac.uk>
2020-05-14 16:02:40 +0200

    Wrote basic qr code generator script (py)

---
+++ qr_generator.py
@@ -0,0 +1,26 @@
+import pyqrcode
+from PIL import Image
+import png
+
+# TODO make into a function with url and image strings + filename of finalprodut
+# TODO add way of iterating over barcodes
+
+# make QR code from custom string
+qrobj = pyqrcode.QRCode('https://fvvianello.github.io/pymol_ar/', error='H')
+with open('test_qr.png', 'wb') as f:
+    qrobj.png(f, scale=10)
+
+# open the QR code to place the logo in the middle
+img = Image.open('test_qr.png')
+width, height = img.size
+
+# open the logo image
+logo = Image.open('06-barcode.png')
+
+# place logo
+logo_size = 150
+xmin = ymin = int((width / 2) - (logo_size / 2))
+xmax = ymax = int((width / 2) + (logo_size / 2))
+logo = logo.resize((xmax - xmin, ymax - ymin))
+img.paste(logo, (xmin, ymin, xmax, ymax))
+img.save('test_qr.png')
